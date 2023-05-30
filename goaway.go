2f9a61fdd20096dfab602ff50cc52d26091c513c
gKits <gkitsikoudis@gmx.de>
2023-05-30 07:08:34 +0200

    feat: overwork authentication flow and configurable functions

--- goaway.go
+++ goaway.go
@@ -15,12 +15,14 @@ type GoAway[U, P interface{}] struct {
 }
 
 type GoAwayFunctions[U, P interface{}] struct {
-	UserFromCredentials             func(string, string) (U, error)
-	UserFromRefreshToken            func(string) (U, error)
-	NewPayloadFromUser              func(U) (P, error)
-	NewRefreshTokenFromUser         func(U) (string, error)
-	ValidateRefreshTokenFromPayload func(string, P) error
-	RevokeRefreshToken              func(string) error
+	UserFromCreds   func(string, string) (U, error) // Finds the user of the given username and returns it if the password matches
+	UserFromRT      func(string) (U, error)         // Finds the corresponding user from the given RT payload and returns it
+	NewPayload      func(U) (P, error)              // Generates an AT payload from the given user and returns it
+	StoreRT         func(U) (string, error)         // Stores the RT payload and returns its unique ID
+	RevokeRT        func(string) error              // Deletes the RT with the given ID
+	BlacklistAT     func(string) error              // Stores the given AT in the blacklist
+	RTIsRevoked     func(string) bool               // Checks weither the RT with the given ID is revoked
+	ATIsBlacklisted func(string) bool               // Checks weither the AT is blacklisted
 }
 
 type LoginRequest struct {
@@ -29,50 +31,39 @@ type LoginRequest struct {
 }
 
 type GoAwayConfig struct {
-	AccessTokenTTL           time.Duration
-	RefreshTokenTTL          time.Duration
-	EnvAccessTokenPrivateKey string
-	EnvAccessTokenPublicKey  string
-	CookieAccessToken        string
-	CookieRefreshToken       string
-	ContextPayload           string
-	CookieDomain             string
-	CookiePath               string
-	CookieHttpOnly           bool
-	CookieSecure             bool
+	AccessTokenTTL            time.Duration
+	RefreshTokenTTL           time.Duration
+	EnvAccessTokenPrivateKey  string
+	EnvAccessTokenPublicKey   string
+	EnvRefreshTokenPrivateKey string
+	EnvRefreshTokenPublicKey  string
+	CookieAccessToken         string
+	CookieRefreshToken        string
+	ContextPayload            string
+	CookieDomain              string
+	CookiePath                string
+	CookieHttpOnly            bool
+	CookieSecure              bool
 }
 
 var DefaultGoAwayConfig = GoAwayConfig{
-	AccessTokenTTL:           15 * time.Minute,
-	RefreshTokenTTL:          24 * time.Hour,
-	EnvAccessTokenPrivateKey: "ACCESS_TOKEN_PRIVATE_KEY",
-	EnvAccessTokenPublicKey:  "ACCESS_TOKEN_PUBLIC_KEY",
-	CookieAccessToken:        "access_token",
-	CookieRefreshToken:       "refresh_token",
-	ContextPayload:           "payload",
-	CookieDomain:             "",
-	CookiePath:               "/",
-	CookieHttpOnly:           true,
-	CookieSecure:             false,
+	AccessTokenTTL:            15 * time.Minute,
+	RefreshTokenTTL:           24 * time.Hour,
+	EnvAccessTokenPrivateKey:  "ACCESS_TOKEN_PRIVATE_KEY",
+	EnvAccessTokenPublicKey:   "ACCESS_TOKEN_PUBLIC_KEY",
+	EnvRefreshTokenPrivateKey: "REFRESH_TOKEN_PRIVATE_KEY",
+	EnvRefreshTokenPublicKey:  "REFRESH_TOKEN_PUBLIC_KEY",
+	CookieAccessToken:         "access_token",
+	CookieRefreshToken:        "refresh_token",
+	ContextPayload:            "payload",
+	CookieDomain:              "",
+	CookiePath:                "/",
+	CookieHttpOnly:            true,
+	CookieSecure:              false,
 }
 
-// Create a new GoAway object with the given User and Payload type and the functions:
-//
-//	UfC: UserFromCredentials: returns the user if username and password are valid
-//	UfRT: UserFromRefreshToken: returns the user by checking the refresh tokens owner
-//	NPfU: NewPayloadFromuser: returns a new payload by generating it from the users data
-//	NRTfU: NewRefreshTokenFromUser: generates a new refresh token from the users data and returns the token string
-//	VRTfP: ValidateRefreshTokenFromPayload: compares the attached data of the refresh token to the payload
-//	RRT: RevokeRefreshToken: revokes the refresh token
-func NewGoAway[U, P interface{}](
-	UfC func(string, string) (U, error),
-	UfRT func(string) (U, error),
-	NPfU func(U) (P, error),
-	NRTfU func(U) (string, error),
-	VRTfP func(string, P) error,
-	RRT func(string) error,
-	configs ...GoAwayConfig,
-) (*GoAway[U, P], error) {
+// Create a new GoAway object with the given User and Payload type and the functions.
+func NewGoAway[U, P interface{}](funcs GoAwayFunctions[U, P], configs ...GoAwayConfig) (*GoAway[U, P], error) {
 	var config *GoAwayConfig
 	if len(configs) > 1 {
 		return nil, fmt.Errorf("GoAway: cannot use multiple configurations")
@@ -80,21 +71,14 @@ func NewGoAway[U, P interface{}](
 		var err error
 		config, err = Merge(configs[0], DefaultGoAwayConfig)
 		if err != nil {
-			return nil, fmt.Errorf("GoAway: could not merge config with default config: %s", err.Error())
+			return nil, fmt.Errorf("GoAway: could not merge config with default config: %e", err)
 		}
 	} else {
 		config = &DefaultGoAwayConfig
 	}
 	return &GoAway[U, P]{
-		GoAwayFunctions: GoAwayFunctions[U, P]{
-			UserFromCredentials:             UfC,
-			UserFromRefreshToken:            UfRT,
-			NewPayloadFromUser:              NPfU,
-			NewRefreshTokenFromUser:         NRTfU,
-			ValidateRefreshTokenFromPayload: VRTfP,
-			RevokeRefreshToken:              RRT,
-		},
-		GoAwayConfig: *config,
+		GoAwayFunctions: funcs,
+		GoAwayConfig:    *config,
 	}, nil
 }
 
@@ -112,26 +96,25 @@ func (g *GoAway[U, P]) Login(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	user, err := g.UserFromCredentials(req.Username, req.Password)
+	user, err := g.UserFromCreds(req.Username, req.Password)
 	if err != nil {
 		JSONResponse(w, http.StatusUnauthorized, ErrInvalidCredentials)
 		return
 	}
 
-	createdAt := time.Now()
-	accessToken, refreshToken, err := g.generateTokenPair(user, createdAt)
+	now := time.Now()
+	accessToken, refreshToken, err := g.generateTokenPair(user, now)
 	if err != nil {
 		JSONResponse(w, http.StatusInternalServerError, ErrFailGenerateTokenPair(err))
 		return
 	}
 
-	http.SetCookie(w, NewCookie(g.CookieAccessToken, accessToken, g.CookieDomain, g.CookiePath, createdAt.Add(g.AccessTokenTTL), g.CookieHttpOnly, g.CookieSecure))
-	http.SetCookie(w, NewCookie(g.CookieRefreshToken, refreshToken, g.CookieDomain, g.CookiePath, createdAt.Add(g.RefreshTokenTTL), g.CookieHttpOnly, g.CookieSecure))
+	http.SetCookie(w, NewCookie(g.CookieAccessToken, accessToken, g.CookieDomain, g.CookiePath, now.Add(g.AccessTokenTTL), g.CookieHttpOnly, g.CookieSecure))
+	http.SetCookie(w, NewCookie(g.CookieRefreshToken, refreshToken, g.CookieDomain, g.CookiePath, now.Add(g.RefreshTokenTTL), g.CookieHttpOnly, g.CookieSecure))
 
 	JSONResponse(w, http.StatusOK, Response{
-		Status:       "success",
-		AccessToken:  accessToken,
-		RefreshToken: refreshToken,
+		Status:      "success",
+		AccessToken: accessToken,
 	})
 }
 
@@ -142,18 +125,36 @@ func (g *GoAway[U, P]) Logout(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	refreshTokenCookie, err := r.Cookie(g.CookieRefreshToken)
+	rtCookie, err := r.Cookie(g.CookieRefreshToken)
 	if err != nil {
 		JSONResponse(w, http.StatusBadRequest, ErrCookieIsMissing(err))
 		return
 	}
-	if err := g.RevokeRefreshToken(refreshTokenCookie.Value); err != nil {
-		JSONResponse(w, http.StatusInternalServerError, ErrFailRevokeRefreshToken(err))
+
+	accessToken, err := AccessTokenFromHeaderOrCookie(r, "Authorization", g.CookieAccessToken)
+	if err != nil {
+		JSONResponse(w, http.StatusBadRequest, nil)
 		return
 	}
 
+	r.Header.Set("Authorization", "")
 	http.SetCookie(w, NewCookie(g.CookieAccessToken, "", g.CookieDomain, g.CookiePath, time.Now(), g.CookieHttpOnly, g.CookieSecure))
 	http.SetCookie(w, NewCookie(g.CookieRefreshToken, "", g.CookieDomain, g.CookiePath, time.Now(), g.CookieHttpOnly, g.CookieSecure))
+
+	rtClaims, err := ValidateJWT[string](rtCookie.Value, g.EnvRefreshTokenPublicKey)
+	if err != nil {
+		JSONResponse(w, http.StatusUnauthorized, nil)
+		return
+	}
+
+	if err := g.RevokeRT(rtClaims.ID); err != nil {
+		JSONResponse(w, http.StatusInternalServerError, ErrFailRevokeRefreshToken(err))
+		return
+	}
+	if err := g.BlacklistAT(accessToken); err != nil {
+		JSONResponse(w, http.StatusInternalServerError, nil)
+		return
+	}
 
 	JSONResponse(w, http.StatusOK, ResSuccessfulLogout)
 }
@@ -171,55 +172,60 @@ func (g *GoAway[U, P]) Refresh(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	user, err := g.UserFromRefreshToken(refreshTokenCookie.Value)
+	accessToken, err := AccessTokenFromHeaderOrCookie(r, "Authorization", g.CookieAccessToken)
+	if err != nil {
+		JSONResponse(w, http.StatusBadRequest, nil)
+		return
+	}
+
+	rtClaims, err := ValidateJWT[string](refreshTokenCookie.Value, os.Getenv(g.EnvRefreshTokenPublicKey))
+	if err != nil {
+		JSONResponse(w, http.StatusUnauthorized, nil)
+		return
+	}
+
+	user, err := g.UserFromRT(rtClaims.ID)
 	if err != nil {
 		JSONResponse(w, http.StatusInternalServerError, ErrInvalidRefreshToken(err))
 		return
 	}
-	if err := g.RevokeRefreshToken(refreshTokenCookie.Value); err != nil {
+
+	if err := g.RevokeRT(refreshTokenCookie.Value); err != nil {
 		JSONResponse(w, http.StatusInternalServerError, ErrFailRevokeRefreshToken(err))
 		return
 	}
-	createdAt := time.Now()
-	accessToken, refreshToken, err := g.generateTokenPair(user, createdAt)
+
+	if err := g.BlacklistAT(accessToken); err != nil {
+
+	}
+
+	now := time.Now()
+
+	accessToken, refreshToken, err := g.generateTokenPair(user, now)
 	if err != nil {
 		JSONResponse(w, http.StatusInternalServerError, ErrFailGenerateTokenPair(err))
 		return
 	}
 
-	http.SetCookie(w, NewCookie(g.CookieAccessToken, accessToken, g.CookieDomain, g.CookiePath, createdAt.Add(g.AccessTokenTTL), g.CookieHttpOnly, g.CookieSecure))
-	http.SetCookie(w, NewCookie(g.CookieRefreshToken, refreshToken, g.CookieDomain, g.CookiePath, createdAt.Add(g.RefreshTokenTTL), g.CookieHttpOnly, g.CookieSecure))
+	http.SetCookie(w, NewCookie(g.CookieAccessToken, accessToken, g.CookieDomain, g.CookiePath, now.Add(g.AccessTokenTTL), g.CookieHttpOnly, g.CookieSecure))
+	http.SetCookie(w, NewCookie(g.CookieRefreshToken, refreshToken, g.CookieDomain, g.CookiePath, now.Add(g.RefreshTokenTTL), g.CookieHttpOnly, g.CookieSecure))
 
 	JSONResponse(w, http.StatusOK, Response{
-		Status:       "success",
-		AccessToken:  accessToken,
-		RefreshToken: refreshToken,
+		Status:      "success",
+		AccessToken: accessToken,
 	})
 }
 
 // Middleware that validates the access token from the cookie and attaches its paylod to the context.
 func (g *GoAway[U, P]) ValidateAccessToken(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		accessTokenCookie, err := r.Cookie(g.CookieAccessToken)
-		if err != nil {
-			JSONResponse(w, http.StatusUnauthorized, ErrCookieIsMissing(err))
-			return
-		}
-		refreshTokenCookie, err := r.Cookie(g.CookieRefreshToken)
-		if err != nil {
-			JSONResponse(w, http.StatusUnauthorized, ErrCookieIsMissing(err))
-		}
+		accessToken, err := AccessTokenFromHeaderOrCookie(r, "Authorization", g.CookieAccessToken)
 
-		claims, err := ValidateAccessToken[P](
-			accessTokenCookie.Value,
+		claims, err := ValidateJWT[P](
+			accessToken,
 			os.Getenv(g.EnvAccessTokenPublicKey))
 		if err != nil {
 			JSONResponse(w, http.StatusUnauthorized, ErrInvalidAccessToken(err))
-			return
-		}
-
-		if err := g.ValidateRefreshTokenFromPayload(refreshTokenCookie.Value, claims.Data); err != nil {
-			JSONResponse(w, http.StatusUnauthorized, ErrInvalidRefreshToken(err))
 			return
 		}
 
@@ -230,23 +236,25 @@ func (g *GoAway[U, P]) ValidateAccessToken(next http.Handler) http.Handler {
 }
 
 // Returns a freshly generated pair of accessToken and refreshToken from the given user and synced timestamp.
-func (g *GoAway[U, P]) generateTokenPair(user U, createdAt time.Time) (string, string, error) {
-	payload, err := g.NewPayloadFromUser(user)
+func (g *GoAway[U, P]) generateTokenPair(user U, now time.Time) (string, string, error) {
+	atPayload, err := g.NewPayload(user)
 	if err != nil {
 		return "", "", err
 	}
-	// TODO: Generate a unique identifier for the token pair
-	accessToken, err := GenerateAccessToken(
-		createdAt.Add(g.AccessTokenTTL),
-		payload,
-		"",
-		os.Getenv(g.EnvAccessTokenPrivateKey))
+
+	refreshTokenID, err := g.StoreRT(user)
 	if err != nil {
 		return "", "", err
 	}
-	refreshToken, err := g.NewRefreshTokenFromUser(user)
+
+	accessToken, err := GenerateJWT(now, g.AccessTokenTTL, atPayload, os.Getenv(g.EnvAccessTokenPrivateKey))
 	if err != nil {
 		return "", "", err
 	}
+	refreshToken, err := GenerateJWT(now, g.RefreshTokenTTL, refreshTokenID, os.Getenv(g.EnvRefreshTokenPrivateKey))
+	if err != nil {
+		return "", "", err
+	}
+
 	return accessToken, refreshToken, nil
 }
