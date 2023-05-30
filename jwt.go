1ac2598dbe41163d9f2730e9895110de35e1d66f
gKits <gkitsikoudis@gmx.de>
2023-05-30 07:06:35 +0200

    form: renaming of JWT functions

--- jwt.go
+++ jwt.go
@@ -9,14 +9,14 @@ import (
 )
 
 type GoAwayClaims[P interface{}] struct {
-	Data P `json:"data"`
+	Data P `json:"data,ommitempty"`
 	jwt.RegisteredClaims
 }
 
-// Returns an access token that contains the given payload of the specified type.
+// Returns an jwt that contains the given payload of the specified type.
 // It expires at the given expiresAt time and is encoded with RS256 using b64 encoded privateKey.
 // The id is used as a unique identifier of the token.
-func GenerateAccessToken[P interface{}](expiresAt time.Time, payload P, id, privateKey string) (string, error) {
+func GenerateJWT[P interface{}](now time.Time, ttl time.Duration, payload P, privateKey string) (string, error) {
 	pemKey, err := base64.StdEncoding.DecodeString(privateKey)
 	if err != nil {
 		return "", err
@@ -30,9 +30,8 @@ func GenerateAccessToken[P interface{}](expiresAt time.Time, payload P, id, priv
 	claims := GoAwayClaims[P]{
 		Data: payload,
 		RegisteredClaims: jwt.RegisteredClaims{
-			ID:        id,
-			ExpiresAt: jwt.NewNumericDate(expiresAt),
-			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			ExpiresAt: jwt.NewNumericDate(now.Add(ttl)),
+			IssuedAt:  jwt.NewNumericDate(now),
 		},
 	}
 	token, err := jwt.NewWithClaims(jwt.SigningMethodRS256, claims).SignedString(key)
@@ -43,9 +42,9 @@ func GenerateAccessToken[P interface{}](expiresAt time.Time, payload P, id, priv
 	return token, nil
 }
 
-// Returns the claims of the given jwt access token after decoding using RSA with the b64 encoded publicKey.
+// Returns the claims of the given jwt after decoding using RSA with the b64 encoded publicKey.
 // The type of the payload in use has to be specified as a generic type.
-func ValidateAccessToken[P interface{}](token, publicKey string) (*GoAwayClaims[P], error) {
+func ValidateJWT[P interface{}](token, publicKey string) (*GoAwayClaims[P], error) {
 	pemKey, err := base64.StdEncoding.DecodeString(publicKey)
 	if err != nil {
 		return nil, err
